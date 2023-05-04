73e99ae02fbc193fcfa288be0152be0a48eb52cc
nalugala-vc <vanessachebukwa@gmail.com>
2023-05-04 23:06:46 +0300

    Left Nav bar completed

---
+++ Frontend/web/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import LeftNavigation from './left_navigation/left_navigation';
+
+function App() {
+  return (
+    <LeftNavigation/>
+  );
+}
+
+export default App;
