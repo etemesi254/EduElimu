1ec370e8fe1ea3391fb9d13c62c2f756a3b73054
nalugala-vc <vanessachebukwa@gmail.com>
2023-05-08 12:31:33 +0300

    Resolved some css issues

---
+++ Frontend/web/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import LeftNavigation from './left_navigation/left_navigation';
+import TopNavigation from './top_navigation/top_navigation';
+import RightNavigation from './right_navigation/right_navigation';
+import HomeContent from './home_content/home_content';
+import CarouselImage from './carousell/carousell';
+import HomePage from './completed_homepage/homepage';
+import MainVideoDisplay from './completed_video_display/video_display';
+import VideoPlayer from './main_video_display/main_video';
+
+function App() {
+  return (
+    <HomePage/>
+   
+  );
+}
+
+export default App;
