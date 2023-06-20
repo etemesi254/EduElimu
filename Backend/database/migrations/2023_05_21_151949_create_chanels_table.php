a1a6e705ef88faa2ce83daed62ba6f06a5e841c3
caleb <etemesicaleb@gmail.com>
2023-06-20 16:28:05 +0300

    backend/database/migrations: Fix spelling errors

--- Backend/database/migrations/2023_05_21_151949_create_chanels_table.php
+++ Backend/database/migrations/2023_05_21_151949_create_chanels_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('chanels', function (Blueprint $table) {
+        Schema::create('channels', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('status');
--- Backend/database/migrations/2023_05_21_152519_create_subscribeds_table.php
+++ Backend/database/migrations/2023_05_21_152519_create_subscribeds_table.php
@@ -11,8 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('subscribeds', function (Blueprint $table) {
-            $table->foreignId('chanel_id')->constrained('chanels');
+        Schema::create('subscribers', function (Blueprint $table) {
+            $table->foreignId('channel_id')->constrained('channels');
             $table->foreignId('user_id')->constrained('users');
             $table->timestamps();
         });
