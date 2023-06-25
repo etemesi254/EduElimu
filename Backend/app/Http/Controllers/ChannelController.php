dd35ccde900c3b79f839290ef187790af8d8d3f6
caleb <etemesicaleb@gmail.com>
2023-06-25 17:32:42 +0300

    Backend/app/Http/Controller: Modify ChannelController to allow banner url uploads

--- Backend/app/Http/Controllers/ChannelController.php
+++ Backend/app/Http/Controllers/ChannelController.php
@@ -8,28 +8,48 @@ use Illuminate\Http\Request;
 
 class ChannelController extends Controller
 {
-    //
-    public function addChannel(Request $request): \Illuminate\Http\JsonResponse
+
+
+    public function addChannel(Request $request)
     {
         $rules = [
             "user_id" => "required",
             "name" => "required",
             "description" => "required",
+            "channel_banner" => "required",
         ];
         try {
             $request->validate($rules);
 
             $data = $request->only("user_id", "name", "description");
+
+            $bannerPath = $this->storeChannelBanner($request);
+
+            if (is_bool($bannerPath)) {
+                // a boolean indicates an error
+                return response()->json(
+                    [
+                        "status" => 500,
+                        "message" => "Could not store video",
+                        "data" => null
+                    ], status: 500);
+            }
+
             $channel = Channel::create([
                 "name" => $data["name"],
                 "status" => 1,
                 "user_id" => $data["user_id"],
                 "subscribers" => 0,
                 "description" => $data["description"],
-                "banner"=>"TEMP"
+                "banner" => $bannerPath
             ]);
 
-            return $channel;
+            return response()->json([
+                'status' => 201,
+                'message' => "Successfully created channel",
+                "data" => $channel,
+
+            ], 201);
 
 
         } catch (\Exception $e) {
@@ -39,5 +59,13 @@ class ChannelController extends Controller
             ], 500);
         }
 
+    }
+
+    //
+
+    public function storeChannelBanner(Request $request): bool|string
+    {
+        // store the video
+        return $request->file("channel_banner")->store("channel_banners");
     }
 }
