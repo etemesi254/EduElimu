bb7703fadef44e3ab57a6847d7f05cdda175f814
nalugala-vc <vanessachebukwa@gmail.com>
2023-07-05 20:17:18 +0300

    All about chanels

--- Frontend/web/src/Chanels/chanel_dashboard/chanel_dash.js
+++ Frontend/web/src/Chanels/chanel_dashboard/chanel_dash.js
@@ -8,8 +8,6 @@ import ChanelCourse from "./chanel_course";
 import ChanelVideos from "./chanel_videos";
 
 function ChanelDashboard(){
-    
- 
   // State to track the scroll position and container dimensions
   const [scrollPosition, setScrollPosition] = useState(0);
   const [containerWidth, setContainerWidth] = useState(0);
@@ -58,110 +56,75 @@ function ChanelDashboard(){
     videoRef.current.pause();
     videoRef.current.currentTime = 0;
   }
-    return <div className="home-image">
-    <div className="chanel-dashboard-img">
-        <img src={process.env.PUBLIC_URL + '/assets/poster (1).jpg'} />
-    </div>
-    <div className="chanel-details">
-        <div className="chanel-profile-pic">
-            <img src={process.env.PUBLIC_URL + '/assets/poster (2).jpg'} />
+    return <div className="chanel-page-content">
+    <div className="intro-video-div">
+        <div className="video-div">
+            <video
+                className="intro-vid" 
+                onMouseOver={playMovie}
+                onMouseOut={stopMovie}
+                ref={videoRef}
+                src={process.env.PUBLIC_URL + '/assets/video (3).mp4'}
+                poster={process.env.PUBLIC_URL + '/assets/poster (3).jpg'}
+                preload='none'
+                loop
+            />
         </div>
-        <div className="chanel-dets">
-            <h2>Chanel Name</h2>
-            <div>
-                <p>1452k Subscribers</p>
-                <p>637 Videos</p>
-                <p>78 Courses</p>
+        <div className="intro-video-deets">
+            <h4>Artifical Intelligence and its effects</h4>
+            <div className="stats">
+                <p>1 year ago</p>
+                <p>256k views</p>
             </div>
-            <p>long ass desciption bla bla</p>
-        </div>
-    </div>
-    <div className="mini-nav-bar">
-        <div className="mini-nav-bar-link">
-            <li>
-                <Link to="" className="link">HOME</Link>
-            </li>
-            <li>
-                <Link to="" className="link">VIDEOS</Link>
-            </li>
-            <li>
-                <Link to="" className="link">COURSES</Link>
-            </li>
-            <li>
-                <Link to="" className="link">ABOUT</Link>
-            </li>
+            <div className="intro-video-details">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit, totam tenetur temporibus unde necessitatibus eveniet nesciunt deleniti molestias iusto magni facere suscipit eius fuga sunt nisi quod laudantium cum consectetur!</p>
+            </div>
         </div>
     </div>
-    <div className="chanel-page-content">
-        <div className="intro-video-div">
-            <div className="video-div">
-                <video
-                    className="intro-vid" 
-                    onMouseOver={playMovie}
-                    onMouseOut={stopMovie}
-                    ref={videoRef}
-                    src={process.env.PUBLIC_URL + '/assets/video (3).mp4'}
-                    poster={process.env.PUBLIC_URL + '/assets/poster (3).jpg'}
-                    preload='none'
-                    loop
-                />
-            </div>
-            <div className="intro-video-deets">
-                <h4>Artifical Intelligence and its effects</h4>
-                <div className="stats">
-                    <p>1 year ago</p>
-                    <p>256k views</p>
-                </div>
-                <div className="intro-video-details">
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit, totam tenetur temporibus unde necessitatibus eveniet nesciunt deleniti molestias iusto magni facere suscipit eius fuga sunt nisi quod laudantium cum consectetur!</p>
-                </div>
-            </div>
+    <div className="scrollable-container">
+        <div className="chanel-video-head">
+            <h2>Videos</h2>
+            <BsFillPlayFill className="scroll-icons"/>
+            <p>view all</p>
         </div>
-        <div className="scrollable-container">
-            <div className="chanel-video-head">
-                <h2>Videos</h2>
-                <BsFillPlayFill className="scroll-icons"/>
-                <p>view all</p>
-            </div>
-            {scrollPosition > 0 && (
-            <div className="scroll-button left-scroll" onClick={scrollLeft}>
-                <BsArrowLeft className="scroll-icons"/>
-            </div>
-            )}
-            <div className="chanel-courses">
-            {videos.map((video,index)=>{
-                return <ChanelVideos video={video} key={video.id}/>
-            })}
-            </div>
-            {scrollPosition < containerScrollWidth - containerWidth && (
-            <div className="scroll-button right-scroll" onClick={scrollRight}>
-                <BsArrowRight  className="scroll-icons"/>
-            </div>
-            )}
+        {scrollPosition > 0 && (
+        <div className="scroll-button left-scroll" onClick={scrollLeft}>
+            <BsArrowLeft className="scroll-icons"/>
         </div>
-        <div className="scrollable-container">
-            <div className="chanel-video-head">
-                <h2>Courses</h2>
-                <BsFillPlayFill className="scroll-icons"/>
-                <p>view all</p>
-            </div>
-            {scrollPosition > 0 && (
-            <div className="scroll-button left-scroll" onClick={scrollLeft}>
-                <BsArrowLeft className="scroll-icons"/>
-            </div>
-            )}
-            <div className="chanel-courses">
-            {videos.map((video,index)=>{
-                return <ChanelCourse video={video} key={video.id}/>
-            })}
-            </div>
-            {scrollPosition < containerScrollWidth - containerWidth && (
-            <div className="scroll-button right-scroll" onClick={scrollRight}>
-                <BsArrowRight  className="scroll-icons"/>
-            </div>
-            )}
+        )}
+        <div className="chanel-courses">
+        {videos.map((video,index)=>{
+            return <ChanelVideos video={video} key={video.id}/>
+        })}
         </div>
+        {scrollPosition < containerScrollWidth - containerWidth && (
+        <div className="scroll-button right-scroll" onClick={scrollRight}>
+            <BsArrowRight  className="scroll-icons"/>
+        </div>
+        )}
     </div>
+    <div className="scrollable-container">
+        <div className="chanel-video-head">
+            <h2>Courses</h2>
+            <BsFillPlayFill className="scroll-icons"/>
+            <p>view all</p>
+        </div>
+        {scrollPosition > 0 && (
+        <div className="scroll-button left-scroll" onClick={scrollLeft}>
+            <BsArrowLeft className="scroll-icons"/>
+        </div>
+        )}
+        <div className="chanel-courses">
+        {videos.map((video,index)=>{
+            return <ChanelCourse video={video} key={video.id}/>
+        })}
+        </div>
+        {scrollPosition < containerScrollWidth - containerWidth && (
+        <div className="scroll-button right-scroll" onClick={scrollRight}>
+            <BsArrowRight  className="scroll-icons"/>
+        </div>
+        )}
     </div>
+</div>
 }
 export default ChanelDashboard;
