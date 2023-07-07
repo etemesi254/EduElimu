265e66e8aa0983a6f29b6d07ecb1a4afeab6dfc6
nalugala-vc <vanessachebukwa@gmail.com>
2023-07-08 01:48:55 +0300

    Interactive games: Loaded games from quizes

---
+++ Frontend/web/src/Interactive games/game_window.js
@@ -0,0 +1,7 @@
+function GameWindow(){
+    return  <div style={{ width: '100%', display: 'flex', flexDirection: 'column', gap: '8px', minHeight: '635px' }}>
+    <iframe src="https://quizizz.com/embed/quiz/635f8bf12ebf160023153251" title="Math: 6th Grade (with new question types) - Quizizz" style={{ flex: '1' }} frameBorder="0" allowFullScreen></iframe>
+    <a href="https://quizizz.com/admin?source=embedFrame" target="_blank" rel="noopener noreferrer">Explore more at Quizizz.</a>
+  </div>
+}
+export default GameWindow; 
