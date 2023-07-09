0217dcb957c3654c40b98fc401f64b099644a248
caleb <etemesicaleb@gmail.com>
2023-07-10 00:57:19 +0300

    Backend: Add CRUD do video categories and channels

--- Backend/app/Http/Controllers/VideoCategoriesController.php
+++ Backend/app/Http/Controllers/VideoCategoriesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\VideoCategories;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use PHPUnit\Exception;
 
 class VideoCategoriesController extends Controller
@@ -36,7 +37,7 @@ class VideoCategoriesController extends Controller
             $video = VideoCategories::create([
                 "name" => $request->name,
                 "description" => $request->description,
-                "status" => $request->status,
+                // "status" => $request->status,
                 "banner" => $imagePath
             ]);
 
@@ -61,11 +62,105 @@ class VideoCategoriesController extends Controller
     public function storeBanner(Request $request): bool|string
     {
         // store the video
-        return $request->file("banner")->store("categories_banner","public");
+        return $request->file("banner")->store("categories_banner", "public");
     }
 
-    public function listAllCategories(Request $request){
+    public function listAllCategories(Request $request)
+    {
 
         return VideoCategories::all();
+    }
+
+    public function listAllVideosForCategory(Request $request)
+    {
+        $rules = [
+            "category_id" => "required|integer",
+        ];
+
+        try {
+            $request->validate($rules);
+            $result = DB::select("select videos.* from videos inner join video_categories vc on videos.category = vc.id where vc.id=" . $request->category_id);
+            $response = [
+                "status" => 200,
+                "message" => "Successful",
+                "data" => $result,
+            ];
+            return response($response, 200);
+
+        } catch (Exception $e) {
+            return response()->json(
+                [
+                    "status" => 422,
+                    "message" => $e->getMessage(),
+                    "data" => null
+                ], status: 422);
+        }
+    }
+
+    public function deleteVideoCategory(Request $request)
+    {
+        $rules = [
+            "id" => "required|integer|exists:video_categories"
+        ];
+        try {
+            $request->validate($rules);
+            $deleteResp = VideoCategories::destroy($request->id);
+            $resp = [
+                "status" => 200,
+                "message" => "Successfully deleted video category",
+            ];
+            return response($resp, 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 422,
+                'message' => $e->getMessage(),
+            ], 422);
+        }
+    }
+
+    public function updateVideoCategory(Request $request)
+    {
+        $rules = [
+            "name" => "required",
+            "description" => "required",
+            "banner" => "required",
+            "id" => "required|exists:video_categories"
+        ];
+
+        try {
+            $request->validate($rules);
+            // store banner image and video
+            $imagePath = $this->storeBanner($request);
+            if (is_bool($imagePath)) {
+                // a boolean indicates an error
+                return response()->json(
+                    [
+                        "status" => 500,
+                        "message" => "Could not store image banner",
+                        "data" => null
+                    ], status: 500);
+            }
+            // store in db
+            $video = tap(VideoCategories::whereId($request->id))->update([
+                "name" => $request->name,
+                "description" => $request->description,
+                // "status" => $request->status,
+                "banner" => $imagePath
+            ])->first();
+
+            return response()->json([
+                'status' => 200,
+                'message' => "Successfully updated video category",
+                "data" => $video,
+            ], 201);
+        } catch (Exception $e) {
+            return response()->json(
+                [
+                    "status" => 422,
+                    "message" => $e->getMessage(),
+                    "data" => null
+                ], status: 422);
+
+        }
     }
 }
