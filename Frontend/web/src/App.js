bba6ab35b47a92103621a5af3a74c5c890fac57c
caleb <etemesicaleb@gmail.com>
2023-07-13 01:25:06 +0300

    Frontend/web/admin: Add admin tables

--- Frontend/web/src/App.js
+++ Frontend/web/src/App.js
@@ -9,13 +9,13 @@ import SlidingForm from './user_auth/register';
 import RegisterUser from './user_auth/register';
 import Loginuser from './user_auth/login';
 import Watchlist from './watchlist/watchlist';
-import { AuthProvider } from './context/AuthContext';
+import {AuthProvider} from './context/AuthContext';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import PrivateRoute from './context/privateRoute';
 import ForgotPassword from './user_auth/forgot_password';
 import SignInWithPhone from './user_auth/signinwithno';
 import CustomReset from './user_auth/customResetPass';
-import { useState } from 'react';
+import {useState} from 'react';
 import AdminLayout from './Admin/AdminLayout';
 import MainDash from './Admin/mainDash';
 import CompleteProfileEmail from './user_auth/completeProfile';
@@ -35,90 +35,96 @@ import CreateChannel from './Chanels/create chanel/create_chanel';
 import UploadVideos from './Chanels/create chanel/upload_videos';
 import GameCategoryDash from './Interactive games/game_category_dash';
 import GameWindow from './Interactive games/game_window';
-import { UserProvider } from './context/UserContext';
+import {UserProvider} from './context/UserContext';
 import ViewChannelList from './Chanels/viewChannelList';
 import EditChannel from './Chanels/editChannels';
 import ViewVideosList from './Chanels/create chanel/viewVideoList';
 import EditVideos from './Chanels/create chanel/edit_video';
+import VideosTables from "./Admin/VideosTables";
 
 
 function App() {
-  const [showLogout, setShowLogout] = useState(false);
-  const [showDelete, setShowDelete] = useState(false);
-  const [showDeleteVideo, setShowDeleteVideo] = useState(false);
+    const [showLogout, setShowLogout] = useState(false);
+    const [showDelete, setShowDelete] = useState(false);
+    const [showDeleteVideo, setShowDeleteVideo] = useState(false);
 
-  const [hideSidebar,setHideSidebar] = useState(false);
+    const [hideSidebar, setHideSidebar] = useState(false);
 
-  const [showDeclinePrompt,setShowDeclinePrompt] = useState(false);
+    const [showDeclinePrompt, setShowDeclinePrompt] = useState(false);
 
-  const hideSideBar = ()=>{
-      return setHideSidebar(!hideSidebar);
-  }
+    const hideSideBar = () => {
+        return setHideSidebar(!hideSidebar);
+    }
 
-  const [completeProfile,setCompleteProfile] = useState(false);
+    const [completeProfile, setCompleteProfile] = useState(false);
 
-  
-  return (
-    <Router>
-      <AuthProvider>
-        <UserProvider>
-          <Routes showLogout={showLogout}
-              setShowLogout={setShowLogout}>
-            <Route path="/" element={<UserLayout  showLogout={showLogout} 
-                setShowLogout={setShowLogout}/>}>
-              <Route exact path='/' element={<HomePage 
-              showLogout={showLogout} 
-              setShowLogout={setShowLogout}
-                completeProfile={completeProfile}
-                setCompleteProfile={setCompleteProfile}
-                showDeclinePrompt={showDeclinePrompt}
-                setShowDeclinePrompt={setShowDeclinePrompt}
-                setShowDelete={setShowDelete}
-                showDelete = {showDelete}
-                />}/>
-                <Route path="/settings" element={<UserSettings showLogout={showLogout} 
-              setShowLogout={setShowLogout}/>}/>
-              <Route path="/profile" element={<UserProfile showLogout={showLogout} 
-              setShowLogout={setShowLogout}/>}/>
-              <Route path="/create_channel" element={<CreateChannel/>}/>
-              <Route path="/edit_channel/:id/:channel" element={<EditChannel/>}/>
-              <Route path="/edit_userVideos/:id/:video" element={<EditVideos/>}/>
-              <Route path='/show_channel_list' element={<ViewChannelList setShowDelete={setShowDelete} showDelete = {showDelete}/>}/>
-              <Route path="/upload_videos" element={<UploadVideos/>}/>
-              <Route path='/show_video_list' element={<ViewVideosList setShowDeleteVideo={setShowDeleteVideo} showDeleteVideo = {showDeleteVideo}/>}/>
-              <Route path='video_player' element={<VideoPlayer/>}/>
-                <Route path="/chanel/:id/:channel" element={<ChanelLayout/>}>
-                  <Route exact path='/chanel/:id/:channel'element={<ChanelDashboard/>}/>
-                  <Route path='videos/:videos' element={<ChanelVideoPage/>}/>
-                  <Route path='courses' element={<ChanelCoursesPage/>}/>
-                  <Route path='about/:about' element={<ChanelAbout/>}/>
-                </Route>
-            </Route>
-            <Route path='/interactive_games' element={<GameSplashScreen/>}/>
-            <Route path='/interactive_games/dashboard' element={<GameLayout/>}>
-                <Route exact path='/interactive_games/dashboard' element={<GameDashboard/>}/>
-                <Route path='/interactive_games/dashboard/:category/:color/:icon' element={<GameCategoryDash/>}/>
-                <Route path='/interactive_games/dashboard/:quiz' element={<GameWindow/>}/>
-            </Route>
-            <Route path='/register' element={<RegisterUser  completeProfile={completeProfile}
-                setCompleteProfile={setCompleteProfile}/>}/>
-            <Route path='/login' Component={Loginuser}/>
-            <Route path='/forgotPassword' Component={ForgotPassword}/>
-            <Route path='/signupwithphone' Component={SignInWithPhone}/>
-            <Route path='/resetPassword' Component={CustomReset}/>
-            <Route path='/adminLogin' Component={LoginAdmin}/>
-            <Route path='/admin' element={<AdminLayout showLogout={showLogout} 
-                setShowLogout={setShowLogout}/>}>
-              <Route exact path='/admin' element={<MainDash showLogout={showLogout} 
-              setShowLogout={setShowLogout}/>}/>
-            </Route>
-            <Route path='/completeProfile' Component={CompleteProfileEmail}/>
-          </Routes>
-        </UserProvider>
-      </AuthProvider>
-    </Router>
-   
-  );
+
+    return (
+        <Router>
+            <AuthProvider>
+                <UserProvider>
+                    <Routes showLogout={showLogout}
+                            setShowLogout={setShowLogout}>
+                        <Route path="/" element={<UserLayout showLogout={showLogout}
+                                                             setShowLogout={setShowLogout}/>}>
+                            <Route exact path='/' element={<HomePage
+                                showLogout={showLogout}
+                                setShowLogout={setShowLogout}
+                                completeProfile={completeProfile}
+                                setCompleteProfile={setCompleteProfile}
+                                showDeclinePrompt={showDeclinePrompt}
+                                setShowDeclinePrompt={setShowDeclinePrompt}
+                                setShowDelete={setShowDelete}
+                                showDelete={showDelete}
+                            />}/>
+                            <Route path="/settings" element={<UserSettings showLogout={showLogout}
+                                                                           setShowLogout={setShowLogout}/>}/>
+                            <Route path="/profile" element={<UserProfile showLogout={showLogout}
+                                                                         setShowLogout={setShowLogout}/>}/>
+                            <Route path="/create_channel" element={<CreateChannel/>}/>
+                            <Route path="/edit_channel/:id/:channel" element={<EditChannel/>}/>
+                            <Route path="/edit_userVideos/:id/:video" element={<EditVideos/>}/>
+                            <Route path='/show_channel_list'
+                                   element={<ViewChannelList setShowDelete={setShowDelete} showDelete={showDelete}/>}/>
+                            <Route path="/upload_videos" element={<UploadVideos/>}/>
+                            <Route path='/show_video_list'
+                                   element={<ViewVideosList setShowDeleteVideo={setShowDeleteVideo}
+                                                            showDeleteVideo={showDeleteVideo}/>}/>
+                            <Route path='video_player' element={<VideoPlayer/>}/>
+                            <Route path="/chanel/:id/:channel" element={<ChanelLayout/>}>
+                                <Route exact path='/chanel/:id/:channel' element={<ChanelDashboard/>}/>
+                                <Route path='videos/:videos' element={<ChanelVideoPage/>}/>
+                                <Route path='courses' element={<ChanelCoursesPage/>}/>
+                                <Route path='about/:about' element={<ChanelAbout/>}/>
+                            </Route>
+                        </Route>
+                        <Route path='/interactive_games' element={<GameSplashScreen/>}/>
+                        <Route path='/interactive_games/dashboard' element={<GameLayout/>}>
+                            <Route exact path='/interactive_games/dashboard' element={<GameDashboard/>}/>
+                            <Route path='/interactive_games/dashboard/:category/:color/:icon'
+                                   element={<GameCategoryDash/>}/>
+                            <Route path='/interactive_games/dashboard/:quiz' element={<GameWindow/>}/>
+                        </Route>
+                        <Route path='/register' element={<RegisterUser completeProfile={completeProfile}
+                                                                       setCompleteProfile={setCompleteProfile}/>}/>
+                        <Route path='/login' Component={Loginuser}/>
+                        <Route path='/forgotPassword' Component={ForgotPassword}/>
+                        <Route path='/signupwithphone' Component={SignInWithPhone}/>
+                        <Route path='/resetPassword' Component={CustomReset}/>
+                        <Route path='/adminLogin' Component={LoginAdmin}/>
+                        <Route path='/admin' element={<AdminLayout showLogout={showLogout}
+                                                                   setShowLogout={setShowLogout}/>}>
+                            <Route exact path='/admin' element={<MainDash showLogout={showLogout}
+                                                                          setShowLogout={setShowLogout}/>}/>
+                            <Route path="/admin/video-logs" Component={VideosTables}></Route>
+                        </Route>
+                        <Route path='/completeProfile' Component={CompleteProfileEmail}/>
+                    </Routes>
+                </UserProvider>
+            </AuthProvider>
+        </Router>
+
+    );
 }
 
 export default App;
--- Frontend/web/src/context/UserContext.js
+++ Frontend/web/src/context/UserContext.js
@@ -1,232 +1,259 @@
-import React, { createContext, useState, useEffect, useContext } from "react";
-import { useAuth } from "./AuthContext";
+import React, {createContext, useState, useEffect, useContext} from "react";
+import {useAuth} from "./AuthContext";
 
 const UserContext = React.createContext();
 
-export function useUserContext(){
+export function useUserContext() {
     return useContext(UserContext)
 }
 
-export function UserProvider ({ children }){
-  const [user, setUser] = useState();
-  const { currentUser } = useAuth();
-  const [firebaseId, setFirebaseId] = useState(null);
-  const [channel, setChannel] = useState([]);
-  const [userVideos, setUserVideos] = useState([]);
-  const [categories, setCategories] = useState([]);
+export function UserProvider({children}) {
+    const [user, setUser] = useState();
+    const {currentUser} = useAuth();
+    const [firebaseId, setFirebaseId] = useState(null);
+    const [channel, setChannel] = useState([]);
+    const [userVideos, setUserVideos] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [allVideos, setAllVideos] = useState([])
 
+    useEffect(() => {
+        //get current user data from api
+        async function getCurrentUser() {
+            try {
+                const email = encodeURIComponent(currentUser.email);
+                const phoneNumber = currentUser.phoneNumber;
 
-  useEffect(() => {
-    //get current user data from api
-    async function getCurrentUser() {
-      try {
-        const email = encodeURIComponent(currentUser.email);
-        const phoneNumber = currentUser.phoneNumber;
+                const url = `http://127.0.0.1:8000/api/getCurrentUser?email=${email}&phone_number=${phoneNumber}`;
 
-        const url = `http://127.0.0.1:8000/api/getCurrentUser?email=${email}&phone_number=${phoneNumber}`;
+                const response = await fetch(url, {
+                    // mode: 'no-cors',
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
+                console.log(response);
 
-        const response = await fetch(url, {
-          // mode: 'no-cors',
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        console.log(response);
-
-        if (response.status === 201) {
-          const user = await response.json();
-          setUser(user.data); // Update the user state
-          setFirebaseId(user.data.firebase_id);
-          getUserChannel(user.data.id);
-          getUserVideos(user.data.id);
-          getVideoCategories();
-        } else {
-          throw new Error("Failed to fetch current user");
+                if (response.status === 201) {
+                    const user = await response.json();
+                    setUser(user.data); // Update the user state
+                    setFirebaseId(user.data.firebase_id);
+                    getUserChannel(user.data.id);
+                    getUserVideos(user.data.id);
+                    getVideoCategories();
+                } else {
+                    throw new Error("Failed to fetch current user");
+                }
+            } catch (error) {
+                console.error("Error:", error.message);
+            }
         }
-      } catch (error) {
-        console.error("Error:", error.message);
-      }
+
+        //get user channels
+        async function getUserChannel($user_id) {
+            try {
+                const url = `http://127.0.0.1:8000/api/channels/getUserChannels/${$user_id}`;
+
+                const response = await fetch(url, {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
+                console.log(response);
+
+                if (response.status === 200) {
+                    const channels = await response.json();
+                    setChannel(channels.data);
+                    console.log("channels", channel);
+                } else {
+                    throw new Error("Failed to fetch users channels");
+                }
+            } catch (error) {
+                console.error("Error:", error.message);
+            }
+        }
+
+        //get current user videos
+        async function getUserVideos($user_id) {
+            try {
+                const url = `http://127.0.0.1:8000/api/videos/${$user_id}`;
+
+                const response = await fetch(url, {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
+                console.log(response);
+
+                if (response.status === 200) {
+                    const videos = await response.json();
+                    setUserVideos(videos.data);
+                    console.log("videos", userVideos);
+                } else {
+                    throw new Error("Failed to fetch users videos");
+                }
+            } catch (error) {
+                console.error("Error:", error.message);
+            }
+        }
+
+        //get category deetails
+        async function getVideoCategories() {
+            try {
+                const url = `http://127.0.0.1:8000/api/categories/all`;
+
+                const response = await fetch(url, {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                });
+                console.log(response);
+                if (response.status === 200) {
+                    const categories = await response.json();
+                    setCategories(categories.data);
+                    console.log("categories", categories);
+                } else {
+                    throw new Error("Failed to fetch video categories");
+                }
+            } catch (error) {
+                console.error("Error:", error.message);
+            }
+        }
+
+
+        if (currentUser) {
+            getCurrentUser();
+        }
+    }, []);
+
+    async function getVideoChannel(channel_id) {
+        try {
+            const url = `http://127.0.0.1:8000/api/videos/channel/${channel_id}`;
+
+            const response = await fetch(url, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+
+            const result = await response.json();
+            console.log(result.message);
+            console.log(response);
+
+            if (response.status === 200) {
+                return result.data; // Use the parsed JSON data from 'result' variable
+            } else {
+                throw new Error("Failed to fetch video's channel");
+            }
+        } catch (error) {
+            console.error("Error:", error.message);
+        }
     }
 
-    //get user channels
-    async function getUserChannel($user_id) {
-      try {
-        const url = `http://127.0.0.1:8000/api/channels/getUserChannels/${$user_id}`;
 
+    async function getCategoryDetails(category_id) {
+        try {
+            const url = `http://127.0.0.1:8000/api/categories/categoryDetails/${category_id}`;
+
+            const response = await fetch(url, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+
+            const result = await response.json();
+            console.log(result.message);
+            console.log(response);
+
+            if (response.status === 201) {
+                return result.data; // Use the parsed JSON data from 'result' variable
+            } else {
+                throw new Error("Failed to fetch video category details");
+            }
+        } catch (error) {
+            console.error("Error:", error.message);
+        }
+    }
+
+
+    async function getAllVideos() {
+        const url = `http://127.0.0.1:8000/api/videos/all`;
         const response = await fetch(url, {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
         });
-        console.log(response);
 
+        const result = await response.json();
+        console.log(result.message);
+        console.log(response);
         if (response.status === 200) {
-          const channels = await response.json();
-          setChannel(channels.data);
-          console.log("channels",channel);
+            setAllVideos(result.data);
+
+            return result.data;
+
         } else {
-          throw new Error("Failed to fetch users channels");
+            throw new Error("Failed to fetch video details");
+
         }
-      } catch (error) {
-        console.error("Error:", error.message);
-      }
+
+
     }
 
-    //get current user videos
-    async function getUserVideos($user_id) {
-        try {
-          const url = `http://127.0.0.1:8000/api/videos/${$user_id}`;
-  
-          const response = await fetch(url, {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-            },
-          });
-          console.log(response);
-  
-          if (response.status === 200) {
-            const videos = await response.json();
-            setUserVideos(videos.data);
-            console.log("videos",userVideos);
-          } else {
-            throw new Error("Failed to fetch users videos");
-          }
-        } catch (error) {
-          console.error("Error:", error.message);
+    function formatDateTime(datetime) {
+        const currentTime = new Date();
+        const timestamp = new Date(datetime);
+
+        const diffInSeconds = Math.floor((currentTime - timestamp) / 1000);
+
+        if (diffInSeconds < 60) {
+            return `${diffInSeconds} second${diffInSeconds !== 1 ? 's' : ''} ago`;
+        } else if (diffInSeconds < 3600) {
+            const diffInMinutes = Math.floor(diffInSeconds / 60);
+            return `${diffInMinutes} minute${diffInMinutes !== 1 ? 's' : ''} ago`;
+        } else if (diffInSeconds < 86400) {
+            const diffInHours = Math.floor(diffInSeconds / 3600);
+            return `${diffInHours} hour${diffInHours !== 1 ? 's' : ''} ago`;
+        } else if (diffInSeconds < 604800) {
+            const diffInDays = Math.floor(diffInSeconds / 86400);
+            return `${diffInDays} day${diffInDays !== 1 ? 's' : ''} ago`;
+        } else if (diffInSeconds < 2592000) {
+            const diffInWeeks = Math.floor(diffInSeconds / 604800);
+            return `${diffInWeeks} week${diffInWeeks !== 1 ? 's' : ''} ago`;
+        } else if (diffInSeconds < 31536000) {
+            const diffInMonths = Math.floor(diffInSeconds / 2592000);
+            return `${diffInMonths} month${diffInMonths !== 1 ? 's' : ''} ago`;
+        } else {
+            const diffInYears = Math.floor(diffInSeconds / 31536000);
+            return `${diffInYears} year${diffInYears !== 1 ? 's' : ''} ago`;
         }
-      }
-
-      //get category deetails
-      async function getVideoCategories() {
-        try {
-          const url = `http://127.0.0.1:8000/api/categories/all`;
-  
-          const response = await fetch(url, {
-            method: "GET",
-            headers: {
-              "Content-Type": "application/json",
-            },
-          });
-          console.log(response);
-          if (response.status === 200) {
-            const categories = await response.json();
-            setCategories(categories.data);
-            console.log("categories",categories);
-          } else {
-            throw new Error("Failed to fetch video categories");
-          }
-        } catch (error) {
-          console.error("Error:", error.message);
-        }
-      }
-
-
-    if(currentUser){
-        getCurrentUser();
     }
-  }, []);
 
-  async function getVideoChannel(channel_id) {
-    try {
-      const url = `http://127.0.0.1:8000/api/videos/channel/${channel_id}`;
-  
-      const response = await fetch(url, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-  
-      const result = await response.json();
-      console.log(result.message);
-      console.log(response);
-  
-      if (response.status === 200) {
-        return result.data; // Use the parsed JSON data from 'result' variable
-      } else {
-        throw new Error("Failed to fetch video's channel");
-      }
-    } catch (error) {
-      console.error("Error:", error.message);
-    }
-  }
-  
 
-  async function getCategoryDetails(category_id) {
-    try {
-      const url = `http://127.0.0.1:8000/api/categories/categoryDetails/${category_id}`;
-  
-      const response = await fetch(url, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-  
-      const result = await response.json();
-      console.log(result.message);
-      console.log(response);
-  
-      if (response.status === 201) {
-        return result.data; // Use the parsed JSON data from 'result' variable
-      } else {
-        throw new Error("Failed to fetch video category details");
-      }
-    } catch (error) {
-      console.error("Error:", error.message);
-    }
-  }
-  
+    const value = {
+        user,
+        channel,
+        userVideos,
+        categories,
+        formatDateTime,
+        getVideoChannel,
+        getCategoryDetails,
+        getAllVideos,
+        allVideos,
 
-  function formatDateTime(datetime) {
-    const currentTime = new Date();
-    const timestamp = new Date(datetime);
-  
-    const diffInSeconds = Math.floor((currentTime - timestamp) / 1000);
-  
-    if (diffInSeconds < 60) {
-      return `${diffInSeconds} second${diffInSeconds !== 1 ? 's' : ''} ago`;
-    } else if (diffInSeconds < 3600) {
-      const diffInMinutes = Math.floor(diffInSeconds / 60);
-      return `${diffInMinutes} minute${diffInMinutes !== 1 ? 's' : ''} ago`;
-    } else if (diffInSeconds < 86400) {
-      const diffInHours = Math.floor(diffInSeconds / 3600);
-      return `${diffInHours} hour${diffInHours !== 1 ? 's' : ''} ago`;
-    } else if (diffInSeconds < 604800) {
-      const diffInDays = Math.floor(diffInSeconds / 86400);
-      return `${diffInDays} day${diffInDays !== 1 ? 's' : ''} ago`;
-    } else if (diffInSeconds < 2592000) {
-      const diffInWeeks = Math.floor(diffInSeconds / 604800);
-      return `${diffInWeeks} week${diffInWeeks !== 1 ? 's' : ''} ago`;
-    } else if (diffInSeconds < 31536000) {
-      const diffInMonths = Math.floor(diffInSeconds / 2592000);
-      return `${diffInMonths} month${diffInMonths !== 1 ? 's' : ''} ago`;
-    } else {
-      const diffInYears = Math.floor(diffInSeconds / 31536000);
-      return `${diffInYears} year${diffInYears !== 1 ? 's' : ''} ago`;
-    }
-  }
-  
-  
+    };
 
-  const value = {
-    user,
-    channel,
-    userVideos,
-    categories,
-    formatDateTime,
-    getVideoChannel,
-    getCategoryDetails
-  };
-
-  return (
-    <UserContext.Provider value={value}>
-      {children}
-    </UserContext.Provider>
-  );
+    return (
+        <UserContext.Provider value={value}>
+            {children}
+        </UserContext.Provider>
+    );
 };
 
 
